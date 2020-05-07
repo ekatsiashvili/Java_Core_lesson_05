bfee458ab03b22265701c9c34d6eb6041638adf1
Elena <katsiashvili.elena@gmail.com>
2020-05-07 20:12:54 +0300

    fix

--- src/ua/lviv/lgs/cupboad.java
+++
@@ -1,5 +0,0 @@
-package ua.lviv.lgs;
-
-public class cupboad {
-   bow b3 = new bow(3);
-}
--- src/ua/lviv/lgs/table.java
+++
@@ -1,8 +0,0 @@
-package ua.lviv.lgs;
-
-public class table {
-	static bow b1 = new bow(1);
-      table(){
-    	  System.out.println("Table()");
-      }
-}
